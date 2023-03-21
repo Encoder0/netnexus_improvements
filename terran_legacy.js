b94829ac2f24119d4148bfd94272b935ca5e6e81
Encoder0 <100240211+Encoder0@users.noreply.github.com>
2023-03-21 16:44:43 -0400

    color change on items

--- terran_legacy.js
+++ terran_legacy.js
@@ -1,23 +1,36 @@
 // ==UserScript==
 // @name         Terran Legacy Center Content
 // @namespace    https://github.com/Encoder0/netnexus_enchancements
-// @version      0.1
+// @version      0.2
 // @description  Center the TL content
 // @author       Encoder0
-// @match        http://www.netnexus.com/tlclassic/*
+// @match        http*://www.netnexus.com/tlclassic/
+// @match        http*://www.netnexus.com/tlclassic/?*
+// @match        http*://www.netnexus.com/tlclassic/Main.cgi
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=netnexus.com
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
-    var parentNode = document.querySelector("html > frameset");
-    parentNode.cols = "*,245,600,*";
-    var leftNode = document.createElement("frame");
-    leftNode.name = "Side";
-    leftNode.src = "about:blank";
-    leftNode.style = "background-color: black;";
-    var rightNode = leftNode.cloneNode()
-    parentNode.prepend(leftNode);
-    parentNode.append(rightNode);
+
+    // Center the content with some empty pages on each side
+    if (window.location.pathname == "/tlclassic/"){
+        let parentNode = document.querySelector("html > frameset");
+        parentNode.cols = "*,245,600,*";
+        let leftNode = document.createElement("frame");
+        leftNode.name = "Side";
+        leftNode.src = "about:blank";
+        leftNode.style = "background-color: black;";
+        let rightNode = leftNode.cloneNode()
+        parentNode.prepend(leftNode);
+        parentNode.append(rightNode);
+    }
+    // Change color of item text
+    else if (window.location.pathname.includes("Main.cgi")){
+        let f = document.querySelector('font[color="blue"]');
+        if (f){
+            f.color = "pink";
+        }
+    }
 })();
